159c2e1e1eecaa0750c06bc410a01469ea6ccc21
hopegipson <gipson.hope@gmail.com>
2021-01-08 00:29:34 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+ const winningYear = array.find( function(s) { return s.result === "W" })
+  if (winningYear){
+    return winningYear.year
+  }
+
+}
